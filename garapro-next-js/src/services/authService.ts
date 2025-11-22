8c6cc911e5b7afd48112f5db0272db0a73ad2ccc
letienks2410 <anhemgameplay@gmail.com>
2025-11-22 10:33:30 +0700

    m

--- garapro-next-js/src/services/authService.ts
+++ garapro-next-js/src/services/authService.ts
@@ -3,7 +3,7 @@ export interface GoogleLoginDto {
 }
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://localhost:7113/api';
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://localhost:7113';
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://localhost:7113';
 
 export interface AuthResponseDto {
   token: string;      // access token JWT
